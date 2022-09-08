510d41a6a24f30da803a97b0dbbf61efaabe5842
angelofdeity <lawani.gk@gmail.com>
2022-09-08 15:56:04 -0700

    Added C file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+/**
+*main - Entry point
+*
+*Return: always 0
+*
+*/
+#include <stdio.h>
+int main(void)
+{
+	char print_text[] = "Programming is like building a multilingual puzzle";
+	puts(print_text);
+
+	return (0);
+}
