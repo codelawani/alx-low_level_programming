b09fcbaf25a6c2f6466fcce095de31665b2d3b68
angelofdeity <lawani.gk@gmail.com>
2022-09-08 18:12:03 -0700

    Added file for task 5

---
+++ 0x00-hello_world/5-print.c
@@ -0,0 +1,14 @@
+/**
+*main - entry point
+*
+*Return: Always 0
+*
+*/
+#include <stdio.h>
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+
+}
