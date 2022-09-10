f5619354d8926ae631a41e9e17908c2713c729f7
angelofdeity <lawani.gk@gmail.com>
2022-09-09 20:32:49 -0700

    Added new file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+/**
+*main - Prints numbers
+*Description - Prints a loop of all single digit numbers in base 10
+*Return: 0
+*/
+#include <stdio.h>
+int main(void)
+{
+	int y;
+
+	for (y = 0 ; y < 10 ; ++y)
+	{
+	printf("%d", y);
+	}
+	printf("\n");
+	return (0);
+}
