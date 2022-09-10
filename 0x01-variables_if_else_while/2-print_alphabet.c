f64212c0d0f5c31d109de926ee71b035feff49ce
angelofdeity <lawani.gk@gmail.com>
2022-09-09 19:20:31 -0700

    Added new file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+/**
+*main - Prints a loop
+*Description - Prints a loop of the 26 letters of the alphabets
+*Return: 0
+*/
+#include <stdio.h>
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; ++x)
+	{
+	putchar (x);
+	}
+	return (0);
+}
