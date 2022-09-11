df8f74c7ccb2e890350638195d892ff820613a76
angelofdeity <lawani.gk@gmail.com>
2022-09-10 17:58:30 -0700

    Added file that prints hexadecimals

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+/**
+*main - Prints numbers
+*Description - Prints digits from 0 to 9
+*Return: 0
+*/
+#include <stdio.h>
+int main(void)
+{
+	int y;
+	char x;
+	for (y = 0 ; y <= 9 ; ++y)
+	{
+	putchar (y % 10 + '0');
+	}
+	for (x = 'a' ; x <= 'f' ; ++x)
+	{
+	putchar (x);
+	}
+	putchar ('\n');
+	return (0);
+}
