22b5c100b41343af14481427b68e7ff2a52d1320
angelofdeity <lawani.gk@gmail.com>
2022-09-10 18:20:58 -0700

    Added file for task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+/**
+*main - Prints numbers
+*Description - Prints digits from 0 to 9
+*Return: 0
+*/
+#include <stdio.h>
+int main(void)
+{
+	int y;
+	int x;
+	int z;
+
+	x = ',';
+	z = ' ';
+	for (y = 0 ; y <= 9 ; ++y)
+	{
+	putchar (y % 10 + '0');
+	putchar (x);
+	putchar (z);
+	}
+	putchar ('\n');
+	return (0);
+}
