8893784fd07428d8755e3c65c97ad4931cbeff79
angelofdeity <lawani.gk@gmail.com>
2022-09-12 10:07:45 -0700

    Added new file for task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,42 +1,43 @@
-/**
-*main - Prints numbers
-*Description - Prints pairs of double digit combinations of 0 to 9.
+#include <stdio.h>
+/*
+*main - Prints text to output.
+*Description - a combination of double digit in pairs
 *Return: 0
 */
-#include <stdio.h>
 int main(void)
 {
+
 	int a;
 	int b;
 	int c;
 	int d;
 
-	for (a = 0 ; a <= 9 ; a++)
-	{
-	for (b = 0 ; b <= 8 ; b++)
-	{
-	for (c = 0 ; c <= 9 ; c++)
-	{
-	for (d = 1 ; d <= 9 ; d++)
-	{
-	if (((c + d) - (a + b)) >= 1)
-	{
-	putchar (a + '0');
-	putchar (b + '0');
-	putchar (' ');
-	putchar (c % 10 + '0');
-	putchar (d % 10 + '0');
-	if (a + b + c + d == 35)
-	{
-	continue;
-	}
-	putchar (',');
-	putchar (' ');
-	}
-	}
-	}
-	}
-	}
-	putchar ('\n');
-	return (0);
+for (a = 0 ; a <= 9 ; a++)
+{
+for (b = 0 ; b <= 8 ; b++)
+{
+for (c = 0 ; c <= 9; c++)
+{
+for (d = 1 ; d < 10 ; d++)
+{
+if (((c + d) - (a + b)) >= 1)
+{
+putchar (a + '0');
+putchar (b + '0');
+putchar (' ');
+putchar (c + '0');
+putchar (d + '0');
+if (a + b + c + d == 35)
+{
+continue;
+}
+putchar (',');
+putchar (' ');
+}
+}
+}
+}
+}
+putchar ('\n');
+return (0);
 }
