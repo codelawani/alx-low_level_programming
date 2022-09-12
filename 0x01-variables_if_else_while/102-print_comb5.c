23a32226d59a6bd88fa42c2adbdf8afdcf886223
angelofdeity <lawani.gk@gmail.com>
2022-09-12 10:20:15 -0700

    fixed errors

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/*
+/**
 *main - Prints text to output.
 *Description - a combination of double digit in pairs
 *Return: 0
@@ -12,32 +12,32 @@ int main(void)
 	int c;
 	int d;
 
-for (a = 0 ; a <= 9 ; a++)
-{
-for (b = 0 ; b <= 8 ; b++)
-{
-for (c = 0 ; c <= 9; c++)
-{
-for (d = 1 ; d < 10 ; d++)
-{
-if (((c + d) - (a + b)) >= 1)
-{
-putchar (a + '0');
-putchar (b + '0');
-putchar (' ');
-putchar (c + '0');
-putchar (d + '0');
-if (a + b + c + d == 35)
-{
-continue;
-}
-putchar (',');
-putchar (' ');
-}
-}
-}
-}
-}
-putchar ('\n');
-return (0);
+	for (a = 0 ; a <= 9 ; a++)
+	{
+	for (b = 0 ; b <= 8 ; b++)
+	{
+	for (c = 0 ; c <= 9; c++)
+	{
+	for (d = 1 ; d < 10 ; d++)
+	{
+	if (((c + d) - (a + b)) >= 1)
+	{
+	putchar (a + '0');
+	putchar (b + '0');
+	putchar (' ');
+	putchar (c + '0');
+	putchar (d + '0');
+	if (a + b + c + d == 35)
+	{
+	continue;
+	}
+	putchar (',');
+	putchar (' ');
+	}
+	}
+	}
+	}
+	}
+	putchar ('\n');
+	return (0);
 }
