0c2923c72a6ef09b00e2335960c87391729cf340
angelofdeity <lawani.gk@gmail.com>
2022-09-14 07:47:47 -0700

    added new file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* add(int, int) - adds two integers
+* @x, @y - Parameters to be added.
+* Return: result of addition.
+*/
+int add(int x, int y)
+{
+	sum = x + y;
+	return (sum);
+}
