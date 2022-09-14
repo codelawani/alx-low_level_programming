44be1424256c7a51972e7b1edef4eb6e64c67454
angelofdeity <lawani.gk@gmail.com>
2022-09-13 17:16:39 -0700

    added new file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - Prints letters
+* Return: 0
+*/
+void print_alphabet_x10(void)
+{
+	int x;
+
+	x = 0;
+	while (x <= 10)
+	{
+	char y;
+
+	y = 'a';
+	while (y <= 'z')
+	{
+	putchar(y);
+	y++;
+	}
+	putchar('\n');
+	x++;
+	}
+}
+
