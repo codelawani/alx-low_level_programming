19bb319bf2089f6f6ddc18fd41d8f010057a343c
angelofdeity <lawani.gk@gmail.com>
2022-09-14 04:01:34 -0700

    Added new file

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+* times_table - Prints 9 timestable.
+* Return: 0
+*/
+void times_table(void)
+{
+	int x, y, z;
+
+	x = 0;
+	while (x <= 9)
+	{
+	y = 0;
+	while (y <= 9)
+	z = x * y;
+	if ((z / 10) != 0)
+	{
+	_putchar(z / 10 + '0');
+	}
+	_putchar(z % 10 + '0');
+	if (y == 9)
+	{
+	break;
+	}
+	putchar(',');
+	putchar(' ');
+	y++;
+	}
+	x++;
+	putchar('\n');
+	}
+	}
+
