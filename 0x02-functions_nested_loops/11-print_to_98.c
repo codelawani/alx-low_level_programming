7f836e1b33059359e4cece18fbaa0eb56b471f6b
angelofdeity <lawani.gk@gmail.com>
2022-09-14 15:29:28 -0700

    Fixed error

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,8 @@
+/**
+* print_to_98 - Prints numbers n to 98
+* @n:first number of each loop
+* return: 0
+*/
+void print_to_98(int n)
+{
+
--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,15 +1,17 @@
 #include "main.h"
 /**
 * _abs - prints absolute value of number
-* @n: is the parameter being used.
+* @r: is the parameter being used.
 * Return: absolute value.
 */
 int _abs(int r)
 {
 	if (r < 0)
 	{
-	int j;
-	j = (-1) * r
-	return(j);
+	return (-1 * r);
+	}
+	else
+	{
+	return (r);
 	}
 }
