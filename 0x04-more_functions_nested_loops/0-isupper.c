23cd97e059a468c9f879ed06bb5461f79caa607f
angelofdeity <lawani.gk@gmail.com>
2022-09-15 09:33:07 -0700

    added new file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _isupper - Prints uppercase letters.
+* @c: character to be checked.
+* Return: 0
+*/
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory contains solutions to tasks I completed while learning and practicing more functions and nested loops.
