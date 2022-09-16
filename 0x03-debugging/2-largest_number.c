c66f4f31e01300407d2aba3e151bafa07480cb20
angelofdeity <lawani.gk@gmail.com>
2022-09-15 21:19:16 -0700

    added new file

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,17 +10,13 @@
 
 int largest_number(int a, int b, int c)
 {
-	int largest;
+	int largest = b;
 
-	if (a > b && a > c)
+	if (a > b)
 	{
 		largest = a;
 	}
-	else if (b > a && b > c)
-	{
-		largest = b;
-	}
-	else if (c > b && c > a)
+	else if (c > b)
 	{
 		largest = c;
 	}
