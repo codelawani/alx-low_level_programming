ad66c9397c253f6d333ff47f0f702b1b7cb39605
angelofdeity <lawani.gk@gmail.com>
2022-09-19 15:25:12 -0700

    added new file

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _puts - prints a string
+* @str: single character type
+*/
+
+void _puts(char *str)
+{
+	int c, i;
+
+	c = 0;
+	i = 0;
+	while (str[i] != 0)
+	{
+	c++;
+	i++;
+	}
+	_putchar(str[i]);
