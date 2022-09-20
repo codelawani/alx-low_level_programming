ad5fd2c3f11b48e3d0d11644a043ae2c0bf427d8
angelofdeity <lawani.gk@gmail.com>
2022-09-20 09:48:13 -0700

    added new file

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,4 +5,20 @@
 */
 void puts_half(char *str)
 {
+	int i, n, x;
 
+	i = 0;
+	n = 0;
+	while (str[i])
+	{
+	i++;
+	n++;
+	}
+	x = n / 2;
+	while (str[x])
+	{
+	_putchar(str[x]);
+	x++;
+	}
+	_putchar('\n')
+}
