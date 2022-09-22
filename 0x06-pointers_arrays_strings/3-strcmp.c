3228a8b07fac1d04d44dddce72f523ca7b73cfd7
angelofdeity <lawani.gk@gmail.com>
2022-09-22 09:01:50 -0700

    added new file

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,10 +6,16 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-	while ((*s1 && *s2) && (*s1 == *s2))
-	{
-		s1++;
-		s2++;
-	}
-	return (*s1 - *s2);
+int i, flag;
+
+flag = 0;
+for (i = 0 ; s1[i] != '\0' && s2[i] != '\0' ; i++)
+{
+if (s1[i] != s2[i])
+{
+flag = s1[i] - s2[i];
+break;
+}
+}
+return (flag);
 }
