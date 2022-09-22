7264d682570a659ca87c5fad4a58ade76931666a
angelofdeity <lawani.gk@gmail.com>
2022-09-22 08:32:06 -0700

    added new file

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+/**
+* _strcmp - Compares two strings
+* @s1: pointer to string 1
+* @s2: pointer to string 2
+* Return: positive, negative zero value
+*/
+int _strcmp(char *s1, char *s2)
+{
+	int i, flag;
+
+	flag = 0;
+	for (i = 0 ; s1[i] != '\0' || s2[i] != '\0' ; i++)
+	{
+	if (s1[i] != s2[i])
+	flag = s1[0] - s2[0];
+	}
+	return (flag);
+}
