5ca779ec845634c180041209bf6fe1a8fc180116
angelofdeity <lawani.gk@gmail.com>
2022-09-26 16:19:56 -0700

    added new file

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+/**
+* _strchr - locates a character in a string
+* @s: Pointer to string
+* @c: Character to be located
+* Return: pointer to first occurence of c
+*/
+
+char *_strchr(char *s, char c)
+{
+while (*s)
+{
+if (*s == c)
+{
+return (s);
+}
+s++;
+}
+return ("NULL");
+}
