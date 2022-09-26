34099357962b4ed20c5164287620b866cca6add2
angelofdeity <lawani.gk@gmail.com>
2022-09-26 16:48:03 -0700

    added new files

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,14 +4,15 @@
 * @c: Character to be located
 * Return: pointer to first occurence of c
 */
-
 char *_strchr(char *s, char c)
 {
-while (*s)
-{
-if (*s == c)
-return (s);
-s++;
-}
-return ('\0');
+	int index;
+
+	for (index = 0; s[index] >= '\0'; index++)
+	{
+		if (s[index] == c)
+			return (s + index);
+	}
+
+	return ('\0');
 }
