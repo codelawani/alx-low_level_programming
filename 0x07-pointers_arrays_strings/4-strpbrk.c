42c67326b0fe62c13f093f656127e8fb868dee2f
angelofdeity <lawani.gk@gmail.com>
2022-09-27 14:55:26 -0700

    added new file

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+/**
+ * _strpbrk - Searches a string for any of a set of bytes.
+ * @s: The string to be searched.
+ * @accept: The set of bytes to be searched for.
+ *
+ * Return: If a set is matched - a pointer to the matched byte.
+ * If no set is matched - NULL.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+	for (i = 0 ; accept[i] ; i++)
+	{
+	if (*s == accept[i])
+	{
+		return (s);
+	break;
+	}
+	else if (accept[i] == '\0')
+		return ('\0');
+	}
+		s++;
+	}
+return (s);
+}
+
