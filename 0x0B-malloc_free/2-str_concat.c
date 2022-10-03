94cacc0c258d1f9df0df59fbcb1257dd57c4cc7f
angelofdeity <lawani.gk@gmail.com>
2022-10-03 16:39:17 -0700

    fixed error

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,13 +12,13 @@ char *str_concat(char *s1, char *s2)
 {
 	char *s12;
 	int i, j, ii, ij;
-	
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 	i = 0;
-	while(s1[i])
+	while (s1[i])
 	i++;
 	j = 0;
 	while (s2[j])
