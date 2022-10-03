0078103103dd4ac173a060d15f7b2851ceb54a3c
angelofdeity <lawani.gk@gmail.com>
2022-10-03 08:21:32 -0700

    added new file

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,14 +10,12 @@
 char *create_array(unsigned int size, char c)
 {
 	char *p;
-	int i;
+	unsigned int i;
 
 	if (size == 0)
 	return ('\0');
 	p = malloc(size * sizeof(char));
 	for (i = 0 ; i < size ; i++)
-	{
 	*(p + i) = c;
-	}
 	return (p);
 }
