a1482da55d5a4a284f08fa8699ad1b325127d216
angelofdeity <lawani.gk@gmail.com>
2022-10-06 10:25:19 -0700

    added new files

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,11 +8,18 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *p;
+	char *filler;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	p = malloc(nmemb * size);
 	if (p == NULL)
 		return (NULL);
+	filler = p;
+	for (i = 0 ; i < nmemb ; i++)
+	{
+		filler[i] = 0;
+	}
 	return (p);
 }
