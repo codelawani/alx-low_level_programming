b24297ee27052b94d105694a26955000bad54021
angelofdeity <lawani.gk@gmail.com>
2022-10-10 12:45:04 -0700

    added new files

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,10 @@
+#ifndef SUM_H
+#define SUM_H
+/**
+* File: 4-sum.h
+* Desc: A file that computes the sum of two numbers
+* @x: first number
+* @y: second number
+*/
+#define SUM(x, y) ((x) + (y))
+#endif
