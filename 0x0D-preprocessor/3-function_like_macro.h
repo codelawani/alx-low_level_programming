0c1836d10e5b48b92a7667c90d88941a3ea47091
angelofdeity <lawani.gk@gmail.com>
2022-10-10 11:49:04 -0700

    added new file

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -3,9 +3,10 @@
 
 /**
 * File: 3-function_like_macro.h
-* Desc: A function that computes the absolute value of 
+* Desc: A function that computes the absolute value of
 *	of a number x.
+* @x: number to be computed
 */
 
-#define ABS(x) ((x < 0) ? (-x) : (x))
+#define ABS(x) ((x) < 0 ? (-x) : (x))
 #endif
