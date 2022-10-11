bdfc8f583cedbe8f62dbefea66b109ed242b8f10
angelofdeity <lawani.gk@gmail.com>
2022-10-11 12:25:16 -0700

    added new files

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,7 @@
 #include "dog.h"
+#include <stdio.h>
 #include <stdlib.h>
+
 /**
 * new_dog - creates new dog
 * @name: pointer to name of dog
@@ -7,17 +9,66 @@
 * @owner: pointer to owner of dog
 * Return: Pointer to new dog created
 */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	struct dog *d;
+	dog_t *doggy;
 
-	d = malloc(sizeof(struct dog));
-	if (d == NULL)
+	if (name == NULL || age < 0 || owner == NULL)
 		return (NULL);
+	doggy = malloc(sizeof(dog_t));
+	if (doggy == NULL)
+		return (NULL);
+	doggy->name = malloc(strlent(name) + 1);
+	if (doggy->name == NULL)
+	{
+		free(doggy);
+		return (NULL);
+	}
+	doggy->owner = malloc(strlent(owner) + 1);
+	if (doggy->owner == NULL)
+	{
+		free(doggy->name);
+		free(doggy);
+		return (NULL);
+	}
 
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	doggy->name = strcopy(doggy->name, name);
+	doggy->age = age;
+	doggy->owner = strcopy(doggy->owner, owner);
 
-	return (d);
+	return (doggy);
+}
+
+/**
+* strlent - Gets length of a string
+* @string: Pointer to string
+* Return: length of string
+*/
+
+int strlent(char *string)
+{
+	int i;
+
+	for (i = 0 ; string[i] ; i++)
+	;
+
+	return (i);
+}
+
+/**
+* strcopy - copies a string
+* @dest: pointer to copy destination
+* @src: Pointer to copy source
+* Return: Pointer to copy destination
+*/
+
+char *strcopy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0 ; src[i] ; i++)
+	dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
 }
