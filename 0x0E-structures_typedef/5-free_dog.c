09201e75cfaf9074ce801a20241d78c13b5a97e4
angelofdeity <lawani.gk@gmail.com>
2022-10-11 12:37:14 -0700

    added new files

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+* free_dog - Free memory allocated to dog struct
+* @d: pointer to dog struct
+*/
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
