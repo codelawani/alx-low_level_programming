1f288c199dafcce0cc06f6ff2ea46e433e5fcf30
angelofdeity <lawani.gk@gmail.com>
2022-10-21 13:45:21 -0700

    add multiple files

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+* _strlen - Gets length of a string
+* @str: string
+* Return: length of string
+*/
+
+int _strlen(const char *str)
+{
+	int len = 0;
+
+	while (str[len])
+	len++;
+
+	return (len);
+}
+
+/**
+* add_node - adds a node to a linked list
+* @head: pointer to head of list
+* @str: string in node
+* Return: address of new element or null if failed
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *n = malloc(sizeof(list_t));
+
+	n->str = strdup(str);
+	n->len = _strlen(str);
+	n->next = *head;
+	*head = n;
+
+	return (n);
+}
