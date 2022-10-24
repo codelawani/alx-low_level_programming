3347ca37e40491cd6d8938abdddefd6dd75db14d
angelofdeity <lawani.gk@gmail.com>
2022-10-24 10:43:16 -0700

    modify file 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -7,7 +7,7 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int count = 0;
-/*	listint_t *current;
+	listint_t *current;
 
 	current = head;
 	while (current != NULL)
@@ -18,13 +18,13 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		current = current->next;
 	}
 
-	return (NULL);*/
+	return (NULL);
 
-	for (count = 0 ; count < index ; count++)
+/*	for (count = 0 ; count < index ; count++)
 	{
 		if (head == NULL)
 			return (NULL);
 		head = head->next;
 	}
-	return (head);
+	return (head);*/
 }
