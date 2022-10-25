d339f67925c1b572e119d6d6159d0363f0659d95
angelofdeity <lawani.gk@gmail.com>
2022-10-25 12:47:16 -0700

    modify line 32 in file: 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		idx--;
 		while (--idx)
 		{
-			if (current->next == NULL)
+			if (current == NULL || current->next == NULL)
 				return (NULL);
 			current = current->next;
 		}
