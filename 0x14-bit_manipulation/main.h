5569c598c82a09d66587af978aaf43cd629fffa5
angelofdeity <lawani.gk@gmail.com>
2022-10-27 21:40:48 -0700

    add new files for task 3

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -8,5 +8,5 @@ void print_binary(unsigned long int n);
 int _putchar(char c);
 unsigned long int reduce(int p);
 int get_bit(unsigned long int n, unsigned int index);
-
+int set_bit(unsigned long int *n, unsigned int index);
 #endif
