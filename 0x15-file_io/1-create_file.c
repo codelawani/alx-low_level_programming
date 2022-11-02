39539fec572ea95130eba37f865e7742446f68ec
angelofdeity <lawani.gk@gmail.com>
2022-11-02 10:47:25 -0700

    add new files for task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* create_file - creates a file
+* @filename: pointer to name of file to be created
+* @textcontent: a NULL terminated string to write to the file
+* Return: 1 on success or -1 on failure
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, f_write, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_TRUNC | O_EXCL | O_CREAT | O_WRONLY, 0600);
+	if (fd < 0)
+		return (-1);
+	if (text_content == NULL)
+		text_content = "";
+
+	while (text_content[len])
+		len++;
+
+	f_write = write(fd, text_content, len);
+	if (f_write < 0)
+		return (-1);
+
+	return (1);
+}
