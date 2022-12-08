8aba55484275b0948c2a1312b492ab33f4ed05d7
lawani.gk@gmail.com <nico>
2022-12-08 15:05:22 +0100

    fix line 21 betty error

--- 0x17-doubly_linked_lists/5-main.c
+++ 0x17-doubly_linked_lists/5-main.c
@@ -23,7 +23,7 @@ int main(void)
     add_dnodeint_end(&head, 402);
     add_dnodeint_end(&head, 1024);
     print_dlistint(head);
-    node = get_dnodeint_at_index(head, -1);
+    node = get_dnodeint_at_index(head, 99);
     if (node == NULL)
 	printf("F\n");
 	else
