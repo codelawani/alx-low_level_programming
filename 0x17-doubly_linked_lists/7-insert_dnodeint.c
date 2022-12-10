20cb563dc8da5c7f1fb5e73d32ee9d372cc358da
lawani.gk@gmail.com <nico>
2022-12-10 22:44:14 +0100

    modify files

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -24,7 +24,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		current = current->next;
 		if (current == NULL)
+		{
+			free(new);
 			return (NULL);
+		}
 	}
 	if (current->next == NULL)
 		return (add_dnodeint_end(h, n));
