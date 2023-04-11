422c233fa7843d6c9062acf5255d945a7ac02292
Nico <lawani.gk@gmail.com>
2023-04-11 13:26:20 +0100

    add binary search C algo

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,5 +1,13 @@
 #include "search_algos.h"
 
+/**
+ * linear_search - searches for a value in an array of integers
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ * Return: index where value is located or -1 if not found
+ */
+
 int linear_search(int *array, size_t size, int value)
 {
         size_t i;
